34bbdb6b7326210a86e5c4ad01c2227b400b4b50
karix_02 <kluczniok.karol@gmail.com>
2022-06-14 01:26:04 +0200

    init

---
+++ CustomersWhoNeverOrdered.sql
@@ -0,0 +1,6 @@
+SELECT
+c.name as 'Customers'
+FROM
+Customers c
+WHERE
+c.id NOT IN (SELECT DISTINCT o.customerId FROM Orders o)
---
+++ DuplicateEmails.sql
@@ -0,0 +1,7 @@
+SELECT
+email
+FROM
+person
+GROUP BY
+email
+HAVING COUNT(email) > 1
